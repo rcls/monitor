517f231345a1c85794d3d41575ec97b8776caf08
Ralph Loader <ralph1loader@gmail.com>
2025-07-29 17:57:21 +1200

    Looks like we need a little more crystal drive.

--- src/cpu.rs
+++ src/cpu.rs
@@ -185,14 +185,14 @@ pub fn init2() {
     rcc.CIER.write(|w| w.LSIRDYIE().clear_bit());
     rcc.CICR.write(|w| w.bits(!0));
 
+    // Reduce drive strength.  (Lowest drive strength appears unreliable).
+    rcc.BDCR.modify(|_,w| w.LSEDRV().B_0x2());
+
     if CONFIG.fll {
         rcc.CR.write(
             |w| w.MSIRANGE().bits(MSIRANGE).MSIRGSEL().set_bit()
                 . MSION().set_bit().MSIPLLEN().set_bit());
     }
-
-    // Reduce drive strength.  (Lowest drive strength appears unreliable).
-    rcc.BDCR.modify(|_,w| w.LSEDRV().B_0x1());
 
     // Enable interrupts.
     unsafe {
